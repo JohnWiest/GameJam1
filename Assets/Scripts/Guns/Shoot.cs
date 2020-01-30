69db79a059909873914705ec4ae031f012c2e112
SamPettus <spettus@ucsb.edu>
2020-01-30 12:31:25 -0800

    sp - added new weapon and updated gun mechanics

---
+++ Assets/Scripts/Guns/Shoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+
+public class Shoot : MonoBehaviour
+ {
+    public Transform player;
+    public KeyCode shootKey = KeyCode.F;
+    public GameObject projectile;
+    public float shootForce;
+    public AudioClip shootNoise;
+    AudioSource audioSource;
+
+
+// Use this for initialization
+    void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+// Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown(shootKey))
+        {
+            audioSource.PlayOneShot(shootNoise);
+            GameObject shot = GameObject.Instantiate(projectile, transform.position, transform.rotation);
+            shot.GetComponent<Rigidbody>().AddForce(transform.forward * shootForce);
+            shot.GetComponent<Rigidbody>().useGravity = false;
+            shot.GetComponent<killProjectile>().player = player;
+            shot.GetComponent<killProjectile>().self = shot.GetComponent<Transform>();
+        }
+    }
+}
--- Assets/Scripts/Guns/ShootRifle.cs
+++ Assets/Scripts/Guns/ShootRifle.cs
@@ -1,34 +1,45 @@
 ﻿using System.Collections;
- using System.Collections.Generic;
- using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class Shoot : MonoBehaviour
- {
+public class ShootRifle : MonoBehaviour
+{
     public Transform player;
     public KeyCode shootKey = KeyCode.F;
     public GameObject projectile;
     public float shootForce;
     public AudioClip shootNoise;
+    public AudioClip reloadNoise;
     AudioSource audioSource;
+    private float timeSinceLastShot;
 
 
-// Use this for initialization
-    void Start ()
+    // Use this for initialization
+    void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        timeSinceLastShot = 2;
     }
 
-// Update is called once per frame
-    void Update ()
+    // Update is called once per frame
+    void Update()
     {
-        if (Input.GetKeyDown(shootKey))
+        timeSinceLastShot += Time.deltaTime;
+        if (Input.GetKeyDown(shootKey) && timeSinceLastShot>=2)
         {
+            timeSinceLastShot = 0;
             audioSource.PlayOneShot(shootNoise);
             GameObject shot = GameObject.Instantiate(projectile, transform.position, transform.rotation);
             shot.GetComponent<Rigidbody>().AddForce(transform.forward * shootForce);
             shot.GetComponent<Rigidbody>().useGravity = false;
             shot.GetComponent<killProjectile>().player = player;
             shot.GetComponent<killProjectile>().self = shot.GetComponent<Transform>();
+            StartCoroutine(playSoundAfter1Seconds());
         }
     }
- }
+    IEnumerator playSoundAfter1Seconds()
+    {
+        yield return new WaitForSeconds(1.4f);
+        audioSource.PlayOneShot(reloadNoise);
+    }
+}
